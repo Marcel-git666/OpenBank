fa98d777fb0bcd64d9853b9258ed56071efd0b32
Marcel Mravec <marcel.mravec@gmail.com>
2025-01-13 08:18:00 +0100

    initial commit

---
+++ OpenBank/OpenBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenBankApp.swift
+//  OpenBank
+//
+//  Created by Marcel Mravec on 13.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct OpenBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OpenBankTests/OpenBankTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenBankTests.swift
+//  OpenBankTests
+//
+//  Created by Marcel Mravec on 13.01.2025.
+//
+
+import Testing
+@testable import OpenBank
+
+struct OpenBankTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
