706c74116d93bfe15c6426a0589d4dad005d41cb
Marcel Mravec <marcel.mravec@gmail.com>
2025-01-13 08:42:14 +0100

    add NetworkManager from previous projects

---
+++ OpenBank/Models/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  OpenBank
+//
+//  Created by Marcel Mravec on 13.01.2025.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
